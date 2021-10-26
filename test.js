9a622e06f4f4b7f5f77e600664a25c78c1d72368
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-10-26 10:54:31 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+dycalendar.draw({
+    target: '#dycalendar',
+    type: 'month',
+    dayformat : 'full',
+    monthformat: 'full',
+    highlighttargetdate: false,
+    highlighttoday:true,
+    prevnextbutton: 'show'
+})
