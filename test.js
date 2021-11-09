d1442534e8142cb75c764e195fa0d3ce3b11d0ff
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-11-09 10:13:44 +0530

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-dycalendar.draw({
-    target: '#dycalendar',
-    type: 'month',
-    dayformat : 'full',
-    monthformat: 'full',
-    highlighttargetdate: false,
-    highlighttoday:true,
-    prevnextbutton: 'show'
-})
